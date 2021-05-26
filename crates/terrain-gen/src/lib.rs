b4e4095108f0ae163b8c9c9450720c82f0532f2c
iMplode nZ <rg@youxplode.com>
2021-05-26 09:19:44 -0700

    Some work on the terrain-gen.

--- crates/terrain-gen/src/lib.rs
+++ crates/terrain-gen/src/lib.rs
@@ -2,6 +2,11 @@ use itertools::Itertools;
 use std::fmt::Debug;
 use std::ops::Sub;
 
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+struct Triangle<P> {
+    verticies: [P; 3],
+}
+
 /// A point on the surface of a [Topology].
 /// Note that the `Sub` implementation should compute the distance between the points.
 trait SurfacePoint: Sub<Output = f64> + Sized + Copy + Debug {
@@ -11,11 +16,11 @@ trait SurfacePoint: Sub<Output = f64> + Sized + Copy + Debug {
         Self::weighted_average(&points.iter().map(|p| (1., *p)).collect::<Vec<_>>()[..])
     }
 
-    fn triangle_area(triangle: [Self; 3]) -> f64;
+    fn triangle_area(triangle: Triangle<Self>) -> f64;
 
-    fn triangle_centroid(triangle: [Self; 3]) -> Self {
+    fn triangle_centroid(triangle: Triangle<Self>) -> Self {
         // TODO: Figure out whether this is valid.
-        Self::average(&triangle)
+        Self::average(&triangle.verticies)
     }
 
     fn centroid(polygon: &[Self]) -> Self {
@@ -25,7 +30,9 @@ trait SurfacePoint: Sub<Output = f64> + Sized + Copy + Debug {
                 .iter()
                 .circular_tuple_windows()
                 .map(|(x, y)| {
-                    let triangle = [*x, *y, center];
+                    let triangle = Triangle {
+                        verticies: [*x, *y, center],
+                    };
                     (
                         Self::triangle_area(triangle),
                         Self::triangle_centroid(triangle),
@@ -36,8 +43,19 @@ trait SurfacePoint: Sub<Output = f64> + Sized + Copy + Debug {
     }
 }
 
-trait Topology {
+struct Partition<Top: Topology> {
+    points: Vec<Top::Point>,
+    /* TODO */
+}
+
+trait Partitioner<Top: Topology> {
+    fn generate_partition(&self, top: Top) -> Partition<Top>;
+
+}
+
+trait Topology: Sized {
     type Point: SurfacePoint;
+    type Partitioner: Partitioner<Self>;
 
     fn random_point(&self) -> Self::Point;
 }
