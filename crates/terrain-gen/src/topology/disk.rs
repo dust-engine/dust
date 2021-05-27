162f19d83ddbe804f94ac0a18bb1d5dda9ba1d37
iMplode nZ <rg@youxplode.com>
2021-05-26 19:08:40 -0700

    Added random point generation.

--- crates/terrain-gen/src/topology/disk.rs
+++ crates/terrain-gen/src/topology/disk.rs
@@ -1,10 +1,23 @@
 use crate::topology::*;
 use derive_new::*;
 use glam::DVec2;
+use rand::rngs::SmallRng;
+use rand::Rng;
+use rand::SeedableRng;
+use std::f64::consts::PI;
+use voronoice::VoronoiBuilder;
 
 #[derive(new)]
 pub struct Disk {
     radius: f64,
+}
+
+impl Disk {
+    fn random_point(&self, rng: &mut impl Rng) -> DVec2 {
+        let r = self.radius * rng.gen::<f64>().sqrt();
+        let theta = rng.gen::<f64>() * 2.0 * PI;
+        DVec2::new(r * theta.cos(), r * theta.sin())
+    }
 }
 
 impl Topology for Disk {
@@ -16,10 +29,26 @@ impl Topology for Disk {
 #[derive(new)]
 pub struct DiskPartitioner {
     num_points: u32,
+    relaxation_iterations: u32,
 }
 
 impl Partitioner<Disk> for DiskPartitioner {
     fn generate_partition(&self, disk: Disk) -> Partition<Disk> {
+        let mut rng = SmallRng::from_entropy();
+        let points = (0..self.num_points)
+            .map(|_| {
+                let point = disk.random_point(&mut rng);
+                voronoice::Point {
+                    x: point.x,
+                    y: point.y,
+                }
+            })
+            .collect::<Vec<_>>();
+        let voronoi_diagram = VoronoiBuilder::default()
+            .set_sites(points)
+            .set_lloyd_relaxation_iterations(self.relaxation_iterations as usize)
+            .build()
+            .unwrap();
         todo!()
     }
 }
