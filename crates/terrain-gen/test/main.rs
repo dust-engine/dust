3df2d547b516266f808b9aaf92282cf1952ee57c
iMplode nZ <rg@youxplode.com>
2021-05-29 19:07:23 -0700

    Created voronoi renderer.

--- crates/terrain-gen/test/main.rs
+++ crates/terrain-gen/test/main.rs
@@ -1,20 +1,36 @@
 use dust_terrain_gen::topology::Partitioner;
 use dust_terrain_gen::*;
+use itertools::Itertools;
 use macroquad::prelude::*;
 use topology::disk::*;
 
 #[macroquad::main("test")]
 async fn main() {
     let disk = Disk::new(1.0);
-    let partitioner = DiskPartitioner::new(10, 0);
-    println!("{:?}", partitioner.partition(disk));
+    let partitioner = DiskPartitioner::new(100, 0);
+    let size = 2.0;
+    let aspect = screen_height() / screen_width();
+    let partition = DiskPartitioner::new(100, 0).partition(disk);
     loop {
         set_camera(&Camera2D::from_display_rect(Rect::new(
-            -1.0, -1.0, 1.0, 1.0,
+            -size,
+            -size * aspect,
+            2.0 * size,
+            2.0 * size * aspect,
         )));
-        clear_background(RED);
+        clear_background(BLACK);
 
-        draw_line(-1.0, -1.0, 1.0, 1.0, 0.35, BLUE);
+        for pos in &partition.cells.position {
+            draw_circle(pos.x as f32, pos.y as f32, 0.01, WHITE);
+        }
+
+        for boundary in &partition.cells.boundary {
+            for ids in boundary.iter().cloned().circular_tuple_windows::<(_, _)>() {
+                let a = partition.boundary_points[ids.0 as usize];
+                let b = partition.boundary_points[ids.1 as usize];
+                draw_line(a.x as f32, a.y as f32, b.x as f32, b.y as f32, 0.01, WHITE);
+            }
+        }
 
         next_frame().await
     }
