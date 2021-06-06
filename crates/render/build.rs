0d0d687cdb16b564bf7668e6e15c8cf64c5041f6
Zhixing Zhang <me@neoto.xin>
2021-06-06 14:45:04 -0400

    Shader headers compile

--- crates/render/build.rs
+++ crates/render/build.rs
@@ -1,17 +1,44 @@
-const GLSL_SHADER_FILES: [&str; 1] = ["./src/ray.comp"];
+use std::path::Path;
+use shaderc::{IncludeCallbackResult, IncludeType, ResolvedInclude};
+
+const GLSL_ENTRY_FILES: [&str; 1] = ["./src/shaders/ray.comp"];
+const GLSL_INCLUDE_DIR: &str = "./src/shaders";
+const MAX_INCLUDE_DEPTH: usize = 10;
+
+fn include_callback(filename: &str, ty: IncludeType, _origin_filename: &str, depth: usize) -> IncludeCallbackResult {
+    if depth > MAX_INCLUDE_DEPTH {
+        panic!("Max include depth {} exceeded", MAX_INCLUDE_DEPTH);
+    }
+    let mut path = Path::new(GLSL_INCLUDE_DIR).to_path_buf();
+    path.push(filename);
+    match std::fs::read_to_string(&path) {
+        Ok(content) => {
+            println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
+            let resolved_name = path.into_os_string().into_string().expect("Path contains invalid characters");
+            Ok(ResolvedInclude {
+                resolved_name,
+                content,
+            })
+
+        },
+        Err(err) => {
+            Err(err.to_string())
+        }
+    }
+}
 
 fn main() {
     use shaderc::{CompileOptions, Compiler, Error, ShaderKind};
     let mut compiler = Compiler::new().unwrap();
     let mut options = CompileOptions::new().unwrap();
     options.set_target_spirv(shaderc::SpirvVersion::V1_3);
+    options.set_include_callback(include_callback);
 
     let mut out_path = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).to_path_buf();
     out_path.push("shaders");
     std::fs::create_dir_all(&out_path).unwrap();
-    out_path.push("shader.spv");
 
-    for &shader_file_path in &GLSL_SHADER_FILES {
+    for &shader_file_path in &GLSL_ENTRY_FILES {
         let path = std::path::Path::new(shader_file_path);
         println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
         let input = std::fs::read_to_string(path).unwrap();
@@ -44,7 +71,7 @@ fn main() {
 
         assert_eq!(*binary_result.as_binary().first().unwrap(), 0x07230203);
 
-        out_path.set_file_name(path.file_name().unwrap());
+        out_path.push(path.file_name().unwrap());
         out_path.set_extension(match stage {
             ShaderKind::Fragment => "frag.spv",
             ShaderKind::Vertex => "vert.spv",
@@ -52,5 +79,6 @@ fn main() {
             _ => unreachable!(),
         });
         std::fs::write(&out_path, &binary_result.as_binary_u8()).unwrap();
+        out_path.pop();
     }
 }
---
+++ crates/render/src/shaders/ray.glsl
@@ -0,0 +1,15 @@
+struct Node {
+    uint16_t occupancy_freemask;
+    uint16_t _padding2;
+    uint children;
+    uint16_t data[8];
+};
+
+struct Ray {
+    vec3 origin;
+    vec3 dir;
+};
+struct Box {
+    vec3 origin;
+    float extent;
+};
