80342b28717fc89bcbbcfba0548c6362a0fe115b
Zhixing Zhang <me@neoto.xin>
2024-04-28 19:27:33 -0700

    Bind TLAS

--- crates/pbr/src/lib.rs
+++ crates/pbr/src/lib.rs
@@ -1,20 +1,29 @@
 mod pipeline;
 
-use bevy::{app::{App, Plugin, PostUpdate, Startup}, ecs::{query::With, schedule::IntoSystemConfigs}, window::PrimaryWindow};
+use bevy::{
+    app::{App, Plugin, PostUpdate, Startup},
+    ecs::{query::With, schedule::IntoSystemConfigs},
+    window::PrimaryWindow,
+};
 pub use pipeline::*;
 use rhyolite::{acquire_swapchain_image, ecs::IntoRenderSystemConfigs, present, RhyoliteApp};
-
 
 pub struct PbrRendererPlugin;
 impl Plugin for PbrRendererPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(PostUpdate, PbrPipeline::prepare_pipeline);
-        app.add_systems(PostUpdate, PbrPipeline::trace_primary_rays.after(PbrPipeline::prepare_pipeline)
-        .after(acquire_swapchain_image::<With<PrimaryWindow>>)
-        .before(present)
-        .with_barriers(PbrPipeline::trace_primary_rays_barrier));
+        app.add_systems(
+            PostUpdate,
+            PbrPipeline::trace_primary_rays
+                .after(PbrPipeline::prepare_pipeline)
+                .after(acquire_swapchain_image::<With<PrimaryWindow>>)
+                .after(rhyolite_rtx::build_tlas::<rhyolite_rtx::DefaultTLAS>)
+                .before(present)
+                .with_barriers(PbrPipeline::trace_primary_rays_barrier),
+        );
 
-        app.add_device_extension::<rhyolite::ash::khr::push_descriptor::Meta>().unwrap();
+        app.add_device_extension::<rhyolite::ash::khr::push_descriptor::Meta>()
+            .unwrap();
     }
     fn finish(&self, app: &mut App) {
         app.init_resource::<PbrPipeline>();
--- crates/pbr/src/pipeline.rs
+++ crates/pbr/src/pipeline.rs
@@ -1,19 +1,35 @@
 use std::{ops::DerefMut, sync::Arc};
 
-use bevy::{asset::Assets, ecs::{query::With, system::{In, Local, Query, Res, ResMut, Resource}, world::FromWorld}, utils::smallvec::SmallVec};
+use bevy::{
+    asset::Assets,
+    ecs::{
+        query::With,
+        system::{In, Local, Query, Res, ResMut, Resource},
+        world::FromWorld,
+    },
+    utils::smallvec::SmallVec,
+};
 use bytemuck::{Pod, Zeroable};
 use rhyolite::{
-    ash::vk, commands::{CommonCommands, ResourceTransitionCommands}, dispose::RenderObject, ecs::{Barriers, RenderCommands}, pipeline::{CachedPipeline, DescriptorSetLayout, PipelineCache, PipelineLayout}, shader::{ShaderModule, SpecializedShader}, staging::UniformBelt, Access, DeferredOperationTaskPool, ImageLike, SwapchainImage
+    ash::vk,
+    commands::{CommonCommands, ResourceTransitionCommands},
+    dispose::RenderObject,
+    ecs::{Barriers, RenderCommands},
+    pipeline::{CachedPipeline, DescriptorSetLayout, PipelineCache, PipelineLayout},
+    shader::{ShaderModule, SpecializedShader},
+    staging::UniformBelt,
+    Access, DeferredOperationTaskPool, ImageLike, SwapchainImage,
 };
 use rhyolite_rtx::{
-    PipelineGroupManager, RayTracingPipeline, RayTracingPipelineBuildInfoCommon, RayTracingPipelineManager, SbtManager
+    PipelineGroupManager, RayTracingPipeline, RayTracingPipelineBuildInfoCommon,
+    RayTracingPipelineManager, SbtManager,
 };
 
 #[derive(Resource)]
 pub struct PbrPipeline {
     layout: Arc<PipelineLayout>,
     manager: PipelineGroupManager<1>,
-    pipelines: SmallVec<[CachedPipeline<RenderObject<RayTracingPipeline>>; 1]>
+    pipelines: SmallVec<[CachedPipeline<RenderObject<RayTracingPipeline>>; 1]>,
 }
 
 impl FromWorld for PbrPipeline {
@@ -39,7 +55,7 @@ impl FromWorld for PbrPipeline {
             vk::PipelineLayoutCreateFlags::empty(),
         )
         .unwrap();
-    let layout = Arc::new(layout);
+        let layout = Arc::new(layout);
 
         let manager = PipelineGroupManager::new([RayTracingPipelineManager::new(
             RayTracingPipelineBuildInfoCommon {
@@ -54,15 +70,18 @@ impl FromWorld for PbrPipeline {
                 stage: vk::ShaderStageFlags::RAYGEN_KHR,
                 shader: assets.load("shaders/primary/primary.rgen"),
                 ..Default::default()
-            },],
+            }],
             vec![],
             vec![],
             pipeline_cache,
         )]);
-        Self { layout, manager, pipelines: SmallVec::new() }
+        Self {
+            layout,
+            manager,
+            pipelines: SmallVec::new(),
+        }
     }
 }
-
 
 #[repr(C)]
 #[derive(Pod, Zeroable, Clone, Copy)]
@@ -77,25 +96,49 @@ impl PbrPipeline {
         mut this: ResMut<Self>,
         pipeline_cache: Res<PipelineCache>,
         shaders: Res<Assets<ShaderModule>>,
-        pool: Res<DeferredOperationTaskPool>
+        pool: Res<DeferredOperationTaskPool>,
     ) {
         if !this.pipelines.is_empty() {
             return;
         }
-        this.pipelines = this.manager.build(&pipeline_cache, &shaders, &pool).unwrap_or_default();
+        // When to call this build method?
+        // 1. On hot reload
+        // 2. On hitgroup addition / removal.
+        this.pipelines = this
+            .manager
+            .build(&pipeline_cache, &shaders, &pool)
+            .unwrap_or_default();
     }
 
     pub fn trace_primary_rays_barrier(
         In(mut barriers): In<Barriers>,
         mut windows: Query<&mut SwapchainImage, With<bevy::window::PrimaryWindow>>,
+        accel_struct: ResMut<rhyolite_rtx::TLASDeviceBuildStore<rhyolite_rtx::DefaultTLAS>>,
     ) {
         let Ok(mut swapchain) = windows.get_single_mut() else {
             return;
         };
-        barriers.transition(swapchain.deref_mut(), Access {
-            stage: vk::PipelineStageFlags2::RAY_TRACING_SHADER_KHR,
-            access: vk::AccessFlags2::SHADER_STORAGE_WRITE,
-        }, false, vk::ImageLayout::GENERAL);
+        let Some(accel_struct) = accel_struct.into_inner().deref_mut() else {
+            return;
+        };
+        barriers.transition(
+            swapchain.deref_mut(),
+            Access {
+                stage: vk::PipelineStageFlags2::RAY_TRACING_SHADER_KHR,
+                access: vk::AccessFlags2::SHADER_STORAGE_WRITE,
+            },
+            false,
+            vk::ImageLayout::GENERAL,
+        );
+        barriers.transition(
+            accel_struct,
+            Access {
+                stage: vk::PipelineStageFlags2::RAY_TRACING_SHADER_KHR,
+                access: vk::AccessFlags2::ACCELERATION_STRUCTURE_READ_KHR,
+            },
+            true,
+            (),
+        );
     }
     pub fn trace_primary_rays(
         mut commands: RenderCommands<'u'>,
@@ -105,7 +148,7 @@ impl PbrPipeline {
         pool: Res<DeferredOperationTaskPool>,
         mut uniform_belt: ResMut<UniformBelt>,
         windows: Query<&SwapchainImage, With<bevy::window::PrimaryWindow>>,
-        mut count: Local<usize>
+        accel_struct: ResMut<rhyolite_rtx::TLASDeviceBuildStore<rhyolite_rtx::DefaultTLAS>>,
     ) {
         let Ok(swapchain) = windows.get_single() else {
             return;
@@ -118,33 +161,49 @@ impl PbrPipeline {
         let Some(pipeline) = pipeline_cache.retrieve(pipeline, &shaders, &pool) else {
             return;
         };
-
-        *count += 1;
-        if (*count < 100) {
+        let Some(accel_struct) = accel_struct.into_inner().deref_mut() else {
+            println!("AS not ready...");
             return;
-        }
+        };
 
         commands.push_descriptor_set(
             &this.layout,
-            0, &[
+            0,
+            &[
                 vk::WriteDescriptorSet {
-                descriptor_type: vk::DescriptorType::STORAGE_IMAGE,
-                dst_binding: 0,
-                ..Default::default()
-                }.image_info(&[
-                    vk::DescriptorImageInfo {
-                        image_view: swapchain.view,
-                        image_layout: vk::ImageLayout::GENERAL,
-                        sampler: vk::Sampler::null(),
-                    }
-                ])
-            ], vk::PipelineBindPoint::RAY_TRACING_KHR);
+                    descriptor_type: vk::DescriptorType::ACCELERATION_STRUCTURE_KHR,
+                    dst_binding: 0,
+                    descriptor_count: 1,
+                    ..Default::default()
+                }
+                .push_next(
+                    &mut vk::WriteDescriptorSetAccelerationStructureKHR::default()
+                        .acceleration_structures(&[accel_struct.raw()]),
+                ),
+                vk::WriteDescriptorSet {
+                    descriptor_type: vk::DescriptorType::STORAGE_IMAGE,
+                    dst_binding: 1,
+                    ..Default::default()
+                }
+                .image_info(&[vk::DescriptorImageInfo {
+                    image_view: swapchain.view,
+                    image_layout: vk::ImageLayout::GENERAL,
+                    sampler: vk::Sampler::null(),
+                }]),
+            ],
+            vk::PipelineBindPoint::RAY_TRACING_KHR,
+        );
 
-        let mut sbt = pipeline.use_on(&mut commands).trace_rays(&mut uniform_belt, &mut commands);
+        let mut sbt = pipeline
+            .use_on(&mut commands)
+            .trace_rays(&mut uniform_belt, &mut commands);
 
-        sbt.bind_raygen(0, &RayGenParams {
-            color: [0.0, 0.0, 0.0, 1.0],
-        });
+        sbt.bind_raygen(
+            0,
+            &RayGenParams {
+                color: [0.0, 0.0, 1.0, 1.0],
+            },
+        );
 
         sbt.trace(0, swapchain.extent());
     }
